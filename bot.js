754c6a7388149b7bec3c82934898e3d8e92e9614
DS-CodeCrunch <dhruv@DS-CodeCrunchs-MacBook-Air.local>
2024-03-14 10:45:19 +0530

    alert-bot

---
+++ bot.js
@@ -0,0 +1,72 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const ethers = require('ethers');
+const fetch = require('node-fetch');
+const TelegramBot = require('node-telegram-bot-api');
+
+const addresses = {
+    factory: "0x45Ef2BA943859fA6dBC03fF32600B9AACeA1Aef0",
+    // factory: "0x0BFbCF9fa4f9C56B0F40a671Ad40E0805A091865",
+}
+
+const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+
+const privateKey = process.env.PRIVATE_KEY;
+
+const provider = new ethers.providers.WebSocketProvider(process.env.WEB_SOCKET_PROVIDER)
+const wallet = new ethers.Wallet(privateKey);
+const account = wallet.connect(provider)
+
+const bot = new TelegramBot(telegramBotToken, { polling: true });
+
+let chatIds = [];
+
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    if (!chatIds.includes(chatId)) {
+        chatIds.push(chatId);
+        console.log(`Added new chat ID: ${chatId}`);
+    }
+});
+
+
+
+const factory = new ethers.Contract(
+    addresses.factory,
+    ['event PairCreated(address indexed token0, address indexed token1, address pair, uint)'],
+    account
+);
+
+factory.on("PairCreated", async (token0, token1, addressPair) => {
+    const message = `New pair created: Token 0: ${token0} Token 1: ${token1} Pair Address: ${addressPair}`;
+    console.log(message);
+
+    await sendMessageToTelegram(message);
+
+}
+)
+
+async function sendMessageToTelegram(message) {
+    const url = `https://api.telegram.org/bot${telegramBotToken}/sendMessage`;
+    const headers = { 'Content-Type': 'application/json' };
+
+    for (const chatId of chatIds) {
+        const body = JSON.stringify({
+            chat_id: chatId,
+            text: message,
+            parse_mode: 'Markdown'
+        });
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: headers,
+                body: body
+            });
+            const data = await response.json();
+            console.log(`Message sent to chat ID ${chatId}:`, data);
+        } catch (error) {
+            console.error(`Error sending message to chat ID ${chatId}:`, error);
+        }
+    }
+}
